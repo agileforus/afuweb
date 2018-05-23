d1b5822f84e2c7395be8d1bd6de9616ee0b74f63
Marcus Floriano <marcus.floriano@gmail.com>
2018-05-23 09:05:28 -0300

    fix position of members

--- src/app/member.ts
+++ src/app/member.ts
@@ -1,5 +1,6 @@
 export class Member {
-  constructor(public name: string, public avatar: string) {
+  constructor(public id: string, public name: string, public avatar: string) {
+    this.id = id;
     this.name = name;
     this.avatar = avatar;
   }
--- src/app/members.service.ts
+++ src/app/members.service.ts
@@ -7,30 +7,30 @@ import {log} from 'util';
 export class MembersService {
 
   avatars = [
-    { 'image': 'avatar-01.svg' },
-    { 'image': 'avatar-02.svg' },
-    { 'image': 'avatar-03.svg' },
-    { 'image': 'avatar-04.svg' },
-    { 'image': 'avatar-05.svg' },
-    { 'image': 'avatar-06.svg' },
-    { 'image': 'avatar-07.svg' },
-    { 'image': 'avatar-08.svg' },
-    { 'image': 'avatar-09.svg' },
-    { 'image': 'avatar-10.svg' },
-    { 'image': 'avatar-11.svg' },
-    { 'image': 'avatar-12.svg' },
-    { 'image': 'avatar-13.svg' },
-    { 'image': 'avatar-14.svg' },
-    { 'image': 'avatar-15.svg' },
-    { 'image': 'avatar-16.svg' },
-    { 'image': 'avatar-17.svg' },
-    { 'image': 'avatar-18.svg' },
-    { 'image': 'avatar-19.svg' },
-    { 'image': 'avatar-20.svg' },
-    { 'image': 'avatar-21.svg' },
-    { 'image': 'avatar-22.svg' },
-    { 'image': 'avatar-23.svg' },
-    { 'image': 'avatar-24.svg' }
+    { 'id': 'av01', 'image': 'avatar-01.svg' },
+    { 'id': 'av02', 'image': 'avatar-02.svg' },
+    { 'id': 'av03', 'image': 'avatar-03.svg' },
+    { 'id': 'av04', 'image': 'avatar-04.svg' },
+    { 'id': 'av05', 'image': 'avatar-05.svg' },
+    { 'id': 'av06', 'image': 'avatar-06.svg' },
+    { 'id': 'av07', 'image': 'avatar-07.svg' },
+    { 'id': 'av08', 'image': 'avatar-08.svg' },
+    { 'id': 'av09', 'image': 'avatar-09.svg' },
+    { 'id': 'av10', 'image': 'avatar-10.svg' },
+    { 'id': 'av11', 'image': 'avatar-11.svg' },
+    { 'id': 'av12', 'image': 'avatar-12.svg' },
+    { 'id': 'av13', 'image': 'avatar-13.svg' },
+    { 'id': 'av14', 'image': 'avatar-14.svg' },
+    { 'id': 'av15', 'image': 'avatar-15.svg' },
+    { 'id': 'av16', 'image': 'avatar-16.svg' },
+    { 'id': 'av17', 'image': 'avatar-17.svg' },
+    { 'id': 'av18', 'image': 'avatar-18.svg' },
+    { 'id': 'av19', 'image': 'avatar-19.svg' },
+    { 'id': 'av20', 'image': 'avatar-20.svg' },
+    { 'id': 'av21', 'image': 'avatar-21.svg' },
+    { 'id': 'av22', 'image': 'avatar-22.svg' },
+    { 'id': 'av23', 'image': 'avatar-23.svg' },
+    { 'id': 'av24', 'image': 'avatar-24.svg' }
   ]
 
   constructor() { }
@@ -38,7 +38,7 @@ export class MembersService {
   findByRoom() {
     const result = [];
     for (const avatar of this.avatars) {
-      result.push(new Member('Marcus Floriano', avatar['image']));
+      result.push(new Member(avatar['id'], 'Marcus Floriano', avatar['image']));
     }
     return result;
   }
